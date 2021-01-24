1779ecef508357d9fe4e4421f586a1e8e3e54465
Carlos Urquiza <webquiza@gmail.com>
2021-01-23 19:45:10 -0800

    refactor js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-jQuery("h1").click(function() {
+$(document).ready(function() {
+$("h1").click(function() {
   alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!");
 });
 
-jQuery("p").click(function() {
+$("p").click(function() {
   alert("This is a paragraph.");
 });
 
-jQuery("img").click(function() {
+$("img").click(function() {
   alert("This is an image.");
+});
 });
